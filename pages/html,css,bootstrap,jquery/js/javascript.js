2497e0df365ca8e0b1617fa869f773fd53dcc06f
xrzy1985 <jamespatterson1985@yahoo.com>
2018-07-19 20:41:38 -0400

    updates

---
+++ pages/html,css,bootstrap,jquery/js/javascript.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+  $("#A").ready(function(){
+    $("section").hide();
+    $("#home").show("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#shade_white_home").click(function(){
+    $("section").hide();
+    $("#top").show("slow");
+    $("#home").show("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#showHome").click(function(){
+    $("#top").hide("slow");
+    $("section").hide();
+    $("#home").show("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#showAbout").click(function(){
+    $("#top").hide("slow");
+    $("section").hide();
+    $("#aboutMe").show("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#showGoals").click(function(){
+    $("#top").hide("slow");
+    $("section").hide();
+    $("#goals").show("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#showProjects").click(function(){
+    $("#top").hide("slow");
+    $("section").hide();
+    $("#projects").show("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#showSkills").click(function(){
+    $("#top").hide("slow");
+    $("section").hide();
+    $("#skills").show("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#showStrengths").click(function(){
+    $("#top").hide("slow");
+    $("section").hide();
+    $("#strengths").show("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#showSkills").click(function(){
+    $("#top").hide("slow");
+    $("section").hide();
+    $("#skills").show("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#showWork").click(function(){
+    $("#top").hide("slow");
+    $("section").hide();
+    $("#work").show("slow");
+
+  });
+});
