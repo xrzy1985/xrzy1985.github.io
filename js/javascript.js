3457bd2e99aab2597c7443a2d557b457b204e9a9
xrzy1985 <jamespatterson1985@yahoo.com>
2018-07-22 03:58:06 -0400

    updates

--- js/javascript.js
+++ js/javascript.js
@@ -7,3 +7,16 @@ $("#menu-toggle").click(function(e) {
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip();
 });
+
+var config = {
+	
+	apiKey: "AIzaSyANNksn5w6ebGP-hinHibwKz1er3UVz7DE",
+    authDomain: "portfolio-xrzy.firebaseapp.com",
+    databaseURL: "https://portfolio-xrzy.firebaseio.com",
+    projectId: "portfolio-xrzy",
+    storageBucket: "portfolio-xrzy.appspot.com",
+    messagingSenderId: "264341910138"
+	
+};
+
+firebase.initializeApp(config);
